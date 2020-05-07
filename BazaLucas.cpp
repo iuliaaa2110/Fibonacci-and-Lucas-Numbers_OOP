8eec56af0d100a484d553c0b0b8cbb5fa4a63f82
iuliaaa2110 <61266008+iuliaaa2110@users.noreply.github.com>
2020-05-08 00:41:47 +0300

    Add files via upload

---
+++ BazaLucas.cpp
@@ -0,0 +1,9 @@
+#include "NrMare.h"
+#include "BazaLucas.h"
+#include "Fibonacci.h"
+
+int BazaLucas::firstTimeL = 0;
+std::vector<NrMare*> BazaLucas::vL;
+
+int BazaLucas::firstTimeF = 0;
+std::vector<NrMare*> BazaLucas::vF;
