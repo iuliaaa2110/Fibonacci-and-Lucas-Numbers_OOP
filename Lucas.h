e7054b8449e6e72b21b8f38868032057f632d9a2
iuliaaa2110 <61266008+iuliaaa2110@users.noreply.github.com>
2020-05-08 00:45:12 +0300

    Update Lucas.h

--- Lucas.h
+++ Lucas.h
@@ -1,26 +1,16 @@
 #ifndef LUCAS_H_INCLUDED
 #define LUCAS_H_INCLUDED
+#include<algorithm>
+#include<vector>
 
 class Lucas: public BazaLucas ///suma de numere Lucas
 {
 public:
 
-    NrMare *unu, *trei;
-
-    Lucas() {
-        unu = new NrMare("1");
-        trei = new NrMare("3");
-    }
-
-    ~Lucas(){
-        delete unu;
-        delete trei;
-    }
-
     void descompunere()
     {
         std::cout<<"Descompunerea numarului "<<*this<<" in suma de numere Lucas este:"<<std::endl;
-        BazaLucas::descompunere(*unu,*trei);
+        BazaLucas::descompunere(vL);
         std::cout<<std::endl;
     }
 
