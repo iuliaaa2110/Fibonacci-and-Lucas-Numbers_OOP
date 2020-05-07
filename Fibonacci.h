53361232cb5019dfab922a4ed317a4cb91bbef91
iuliaaa2110 <61266008+iuliaaa2110@users.noreply.github.com>
2020-05-08 00:44:44 +0300

    Update Fibonacci.h

--- Fibonacci.h
+++ Fibonacci.h
@@ -1,23 +1,16 @@
 #ifndef FIBONACCI_H_INCLUDED
 #define FIBONACCI_H_INCLUDED
+#include<algorithm>
+#include<vector>
 
 class Fibonacci: public BazaLucas ///suma de numere fibonacci
 {
 public:
-    NrMare *unu;
-
-    Fibonacci() {
-        unu = new NrMare("1");
-    }
-
-    ~Fibonacci(){
-        delete unu;
-    }
 
     virtual void descompunere()
     {
         std::cout<<"Descompunerea numarului "<<*this<<" in suma de numere Fibonacci este:"<<std::endl;
-        BazaLucas::descompunere(*zero,*unu);    //pe zero il mosteneste din BazaLucas
+        BazaLucas::descompunere(vF);    //vF e declarat in Baza
         std::cout<<std::endl;
     }
 };
