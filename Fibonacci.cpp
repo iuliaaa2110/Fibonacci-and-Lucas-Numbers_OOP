4dfc95f1c1bb5bc3be45c863009280eee02f56aa
iuliaaa2110 <61266008+iuliaaa2110@users.noreply.github.com>
2020-05-11 13:30:33 +0300

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -11,8 +11,5 @@ Fibonacci::Fibonacci(){
         vF.push_back(new NrMare("1"));
         firstTimeF = 1;
     }
-
-/*    for(int i=0;i<v.size();i++)
-        std::cout<<v[i]<<" ";
-    std::cout<<std::endl;*/
+    
 }
