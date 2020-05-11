089f38df3ff7301ce5edbe4946bebf1bc47a30f4
iuliaaa2110 <61266008+iuliaaa2110@users.noreply.github.com>
2020-05-11 13:25:11 +0300

    Update BazaLucas.h

--- BazaLucas.h
+++ BazaLucas.h
@@ -1,23 +1,15 @@
 #ifndef BAZALUCAS_H_INCLUDED
 #define BAZALUCAS_H_INCLUDED
-
 #include<vector>
+#include "NrMare.h"
 
 class BazaLucas:public NrMare
 {
     protected:
 
-    static std::vector<NrMare*> vL;
-    static int firstTimeL;
-
-    static std::vector<NrMare*> vF;
-    static int firstTimeF;
-
-    BazaLucas();
-
-    int find_(std::vector<NrMare*> v, NrMare x);    //imi returneaza pozitia celui mai mare element<=x in vector
+    int find_(std::vector<NrMare*> &v, NrMare x);    //imi returneaza pozitia celui mai mare element<=x in vector
     void generate_(std::vector<NrMare*> &v,NrMare n,int i);
-    void descompunere(std::vector<NrMare*> v);
+    void descompunere(std::vector<NrMare*> &v);
 };
 
 #endif // BAZALUCAS_H_INCLUDED
