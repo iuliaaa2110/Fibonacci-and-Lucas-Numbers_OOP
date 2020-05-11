2f95a72c1438f2cf833679337cfa80ce8c93ee4a
iuliaaa2110 <61266008+iuliaaa2110@users.noreply.github.com>
2020-05-11 11:35:46 +0300

    Update BazaLucas.h

--- BazaLucas.h
+++ BazaLucas.h
@@ -13,63 +13,11 @@ class BazaLucas:public NrMare
     static std::vector<NrMare*> vF;
     static int firstTimeF;
 
-    BazaLucas()
-    {
+    BazaLucas();
 
-        if (firstTimeL == 0)
-        {
-            vL.push_back(new NrMare("1"));
-            vL.push_back(new NrMare("3"));
-            firstTimeL = 1;
-        }
-
-         if (firstTimeF == 0)
-        {
-            vF.push_back(new NrMare("0"));
-            vF.push_back(new NrMare("1"));
-            firstTimeF = 1;
-        }
-    }
-
-    int find_(std::vector<NrMare*> v, NrMare x)    //imi returneaza pozitia celui mai mare element<=x in vector
-    {
-        for(int i=0;i<v.size();i++)
-            if(x<*v[i])  //daca am depasit valoarea lui x
-                return i-1;
-
-        return v.size()-1;
-    }
-
-    void generate_(std::vector<NrMare*> &v,NrMare n,int i)
-    {
-        NrMare a=*v[v.size()-2], b=*v[v.size()-1], c;
-
-        while(b<n)
-        {
-            c=a+b;
-            a=b;
-            b=c;
-            v.push_back(new NrMare(b));
-        }
-
-    }
-
-    void descompunere(std::vector<NrMare*> v)
-    {
-        NrMare *zero=new NrMare("0");
-        NrMare n=*this;
-        int i=find_(v,n);
-
-        if(i==v.size()-1)   //inseamna ca nu am toate numerele generate pana la n
-            generate_(v,n,i);
-
-        while(*zero<n)
-        {
-            i=find_(v,n);
-            n=n-*v[i];
-            std::cout<<*v[i]<<" ";
-        }
-    }
+    int find_(std::vector<NrMare*> v, NrMare x);    //imi returneaza pozitia celui mai mare element<=x in vector
+    void generate_(std::vector<NrMare*> &v,NrMare n,int i);
+    void descompunere(std::vector<NrMare*> v);
 };
 
 #endif // BAZALUCAS_H_INCLUDED
